ad098047556fb1ec05f2660a1725cc953e2e8a00
Hodman Ahmed <hodman003@Hodmans-MacBook-Air.local>
2022-08-13 15:55:18 -0500

    Script Start

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 var APIKey = "29f3c58f144182999550f4a430669b7d"
+var city = "";
+var currentDate = "";
+var tempF = "";
+var humidityValue = "";
+var windSpeed = "";
+var minTempK = "";
+var maxTempK = "";
+var minTempF = "";
+var maxTempF = "";
+var dayhumidity = "";
+var currentWeatherIconCode = "";
+var currentWeatherIconUrl = "";
+var iconcode = "";
+var iconurl = "";
+var country = "";
+
+var listOfSearchedCities = [];
+
+var getSearchedCities = JSON.parse(localStorage.getItem("searched-cities"));
+if (getSearchedCities!== null) {
+  getSearchedCities.forEach(function(city) {city.toUpperCase();});
+  listOfSearchedCities = getSearchedCitiesFromLS;  
+}
+
+$(document).ready(function(){
+    displayCities(listOfSearchedCities);
+    if (getSearchedCities !== null) {
+      var lastCity = listOfSearchedCities[0];
+      searchCity(lastCity);
+    }
+});
